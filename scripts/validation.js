57d03935ffb9474f1ee6c6c9f8743a50bf8144b2
Shaheer Azam <muhammadshaheer645@gmail.com>
2023-06-21 14:11:36 +0500

    issues resolved which were mentioned in PR.

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,11 +1,10 @@
 function validate() {
-    var email = document.loginform.email.value;
-    var password = document.loginform.password.value;
-    if (email == "test@gmail.com" && password == "123") {
-      window.open("/portfolio.html");
-    } else {
-      alert("Invalid username or password");
-      return false;
-    }
+  var email = document.loginform.email.value;
+  var password = document.loginform.password.value;
+  if (email == "test@gmail.com" && password == "123") {
+    window.open("/portfolio.html");
+  } else {
+    alert("Invalid username or password");
+    return false;
   }
-  
+}
